f7b1995c006d8f58cdcf85e239ce8b766310bdb7
kyun <kyun9911@gmail.com>
2026-03-03 22:15:58 +0900

    - 레디스 config 추가

--- src/main/java/kr/hhplus/be/server/global/config/RedisConfig.java
+++ src/main/java/kr/hhplus/be/server/global/config/RedisConfig.java
@@ -1,9 +1,14 @@
 package kr.hhplus.be.server.global.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -13,5 +18,16 @@ public class RedisConfig {
             RedisConnectionFactory connectionFactory
     ) {
         return new StringRedisTemplate(connectionFactory);
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30)) // TTL 설정
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(cacheConfig)
+                .build();
     }
 }
